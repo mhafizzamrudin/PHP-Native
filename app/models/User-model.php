7cc2458bc42fe0ea99071a18f1717fd72b6cb5c3
mhafizzamrudin <hafizzamrudin92@gmail.com>
2020-11-02 17:24:55 +0700

    firt commit

---
+++ app/models/User-model.php
@@ -0,0 +1,10 @@
+<?php 
+
+class User_model{
+	private $nama = 'hafiz';
+
+	public function getUser()
+	{
+		returm $this->nama;
+	}
+}
---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+	<h1 class="mt-4">About me</h1>
+	<img src="<?= BASEURL; ?>/img/ibra.jpg" alt="Zlatan Ibrahimovic" widht="200" class="rounded-circle">
+	<p>Hallo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya seorang <?= $data['pekerjaan']; ?></p>
+</div>
